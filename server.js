2cae9a6250ac85531aa8466486b2424df5fb69ad
keba92 <61150239+keba92@users.noreply.github.com>
2020-12-16 20:00:58 +0200

    first commit

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use(bodyParser.urlencoded({
 }));
 
 if (process.env.NODE_ENV === 'production'|| process.env.NODE_ENV === 'staging') {
-  app.use(express.static(path.join(__dirname,'tic-tac/build')));
+  app.use(express.static(path.join(__dirname,'client/build')));
   app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname, 'tic-tac/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 };
 
